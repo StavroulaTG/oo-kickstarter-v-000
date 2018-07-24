44f20ab038f7d04796ef434803ee1622c7f51cc5
Stavroula Guerrero <4stacyg@optonline.net>
2018-07-24 06:14:37 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,12 @@
 class Backer
-  attr_accessor :name
-
-  @@all = []
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
-  def back_project(name)
-    self << @@all
+  def back_project(project)
+    self.backed_projects << project
   end
 end
