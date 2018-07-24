7032369de80be60547ca0454ad2158a6c9a7170d
Stavroula Guerrero <4stacyg@optonline.net>
2018-07-24 06:17:07 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,12 @@
 class Project
   attr_accessor :title
+  
   def initialize(title)
     @title = title
+    @backer = []
+  end
+
+  def add_backer(backer)
+    self.add_backer << backer
   end
 end
